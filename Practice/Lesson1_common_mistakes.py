e089931f992aa0a6bfa0bd2c120e16a9777a5e36
OMUBABY <omubosodienye9@gmail.com>
2025-05-21 13:43:45 +0100

    practice Lesson1_common_mistakes

---
+++ Practice/Lesson1_common_mistakes.py
@@ -0,0 +1,17 @@
+#print('Let's practice finding errors!')
+print('Let\'s practice finding errors!')
+
+#  print('Let's practice finding errors!')
+print('Let\'s practice finding errors!')
+        
+#print'What's wrong with this line?'
+print('What\'s wrong with this line?')
+
+#Print('Fix me')
+print('Fix me')
+
+#print('What is missing here?'
+print('What is missing here?')
+      
+#      print("What do I use" +, + "incorrectly here?")
+print("What do I use", "incorrectly here?")
