9e3a0fbe5a52c3186abd49bd355688ed573928e6
OMUBABY <omubosodienye9@gmail.com>
2025-05-27 11:39:06 +0100

    modified

--- Practice/Lesson1_common_mistakes.py
+++ Practice/Lesson1_common_mistakes.py
@@ -1,17 +1,10 @@
-#print('Let's practice finding errors!')
-print('Let\'s practice finding errors!')
-
-#  print('Let's practice finding errors!')
-print('Let\'s practice finding errors!')
-        
-#print'What's wrong with this line?'
-print('What\'s wrong with this line?')
-
-#Print('Fix me')
-print('Fix me')
-
-#print('What is missing here?'
-print('What is missing here?')
-      
-#      print("What do I use" +, + "incorrectly here?")
-print("What do I use", "incorrectly here?")
+{
+ "cells": [],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
