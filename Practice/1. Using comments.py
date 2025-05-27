e1f0170e1c75191c650c9679bc3772ede70263f3
OMUBABY <omubosodienye9@gmail.com>
2025-05-27 14:17:28 +0100

    added using comments

---
+++ Practice/1. Using comments.py
@@ -0,0 +1,43 @@
+# This is a single line command
+x = 10 #Assign the value of 10 to the variable x
+y = 20 #Assign the value of 20 to the variable y
+
+# Calculate the sume of x + y
+result = x + y
+
+print(result)
+
+
+# This is a multi-line comment
+#created by using multiple
+#single-line comments.
+
+"""
+This is a multi-line comment
+created by using string literals using double quotes.
+"""
+
+'''
+This is a multi-line comment
+created by using string literals using single quotes.
+'''
+# Commented out code
+# This is a single line command
+# x = 10 #Assign the value of 10 to the variable x
+# y = 20 #Assign the value of 20 to the variable y
+
+# # Calculate the sume of x + y
+# result = x + y
+
+# print(result)
+
+# Pycharm shortcuts
+
+# Crtl + /
+# To comment code, place the caret at the appropriate line and press Ctrl + /
+# Press Ctrl + / again on the same line to uncomment it.
+
+# Shift + Alt + . to make the font size larger
+# Shift + Alt + , to make the font size smaller
+# Crtl + D to duplicate a line
+
