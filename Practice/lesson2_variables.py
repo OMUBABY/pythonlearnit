21292886f04d53bba361f7b5f531747486aceab2
OMUBABY <omubosodienye9@gmail.com>
2025-06-02 15:08:49 +0100

    practice

---
+++ Practice/lesson2_variables.py
@@ -0,0 +1,26 @@
+# 1. Create a variable named 'age' and assign it to your age as an integer.
+age = 24
+# 2. Create a variable called 'height" and assign it to your height in meters as a float.
+height = 1.64
+# 3. Create a variable called 'name' and assign it to your name as a string.
+name = "Sodienye Omubo"
+# 4. Create a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not.
+is_student = False
+# 5. Print out all these variable.
+print(f'Original Value:\nAge: {age},\nHeight: {height} meters,\nName: {name},\nIs Student: {is_student}')
+print()
+# 6. Increase the 'age' varable by 1 and print it out.
+age += 1
+print(age)
+print()
+# 7. Multiply the 'height' variable by 100 to convert it to centimeters and print it out. 
+height *= 100
+print(height,"cm")
+print()
+# 8. Add "is my name" to the 'name' variable and print it out.
+name += ' is my name'
+print(name)
+print()
+# 9. Change 'Is_student' to the opposite and print it out.
+is_student = True
+print(is_student)
