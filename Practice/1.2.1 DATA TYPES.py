de6f7bcbb7852af8c045da8e77ff14edfcaa621e
OMUBABY <omubosodienye9@gmail.com>
2025-06-02 13:03:00 +0100

    data types cont.

--- Practice/1.2.1 DATA TYPES.py
+++ Practice/1.2.1 DATA TYPES.py
@@ -5,3 +5,15 @@
 3.14, -0.12 # Floats(float)
 # Text
 # String(str)
+"This is a string" # String(str)
+# Boolean
+True, False # boolean(bool)
+
+age = 27 # age is an integer
+pi = 3.14 # pi is float
+name = 'Helen' # name is a string(str)
+is_new = True # is_new is a boolean(bool)
+
+# Check data type
+# type()
+print(type(age)) #check age variable with type()
