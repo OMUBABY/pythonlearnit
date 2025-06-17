845516c37ec78e882efe4b5b429214e94e365d01
OMUBABY <omubosodienye9@gmail.com>
2025-06-17 12:05:21 +0100

    strings

---
+++ Practice/strings.py
@@ -0,0 +1,12 @@
+message = 'Python'
+
+print(len(message))
+
+print(message[0])
+print(message[1])
+
+fav_color = input('What is your favorite color?: ').lower()
+print(fav_color)
+print()
+fav_color = input('What is your favorite color?: ')
+print(fav_color.upper())
