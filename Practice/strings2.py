999acea2c26cd72360a05c0d5d3daf693fdfcfc7
OMUBABY <omubosodienye9@gmail.com>
2025-09-04 14:47:46 +0100

    string2

---
+++ Practice/strings2.py
@@ -0,0 +1,12 @@
+# Strings in python
+greeting = "Hello, World!"
+name = 'Alice'
+
+# Length of the string
+# Using len() to get the length of a string
+print(len(greeting))  # Output: 13
+
+#Indexing
+print(greeting[0])  # Output: H
+print(greeting[2])  # Output: l
+print(name[-1])      # Output: e (last character)
